12162fdf6ae653bbe79d457455c66635c3a5694b
Pedro Regis <pedroalencarregis@hotmail.com>
2017-01-22 17:01:37 -0300

    Adição do django.template.context_processors.media
    Link de onde achei uma resposta:
    http://stackoverflow.com/questions/3756841/django-media-url-blank

--- project/apploader/views.py
+++ project/apploader/views.py
@@ -15,7 +15,30 @@ def mostra_imagem(request):
 	
 	criaImagem(path, image_name)
 
-	return render(request, 'apploader/mytemplate.html')
+#	return render(request, 'apploader/mytemplate.html')
+	path_page = 'apploader/mytemplate.html'
+	path_imagem = path + image_name
+	return render(request, path_page)#, {"imagem":path_imagem})
+
+
+
+
+"""
+    path_imagem = 'static/projeção_populacional/nome_figura.png'
+    #image_data = open(path, "rb").read()
+    #return HttpResponse(image_data, content_type="image/png")
+    #d = {"imagem":image_data}
+    path_page = 'project/projeção_populacional/results_projeção_populacional.html'
+    return render(request, path_page, {"imagem":path_imagem})
+"""
+
+
+
+
+
+
+
+
 
 """
 def mostra_imagem(request):
--- project/project/settings.py
+++ project/project/settings.py
@@ -63,6 +63,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media', # Adicionado.
+                'django.template.context_processors.static',
             ],
         },
     },
